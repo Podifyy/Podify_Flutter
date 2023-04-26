083bc720d6856efab4e24815e36e1c7433452c33
SHRE-YANSH <itnakim161@gmail.com>
2023-04-26 10:50:22 +0530

    add side nav in home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,7 +5,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:podify/providers/user_provider.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  final String? title;
+  const HomeScreen({super.key, this.title});
   static const String routeName = '/home-screen';
 
   @override
@@ -13,27 +14,65 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final routes = List.generate(20, (i) => 'test $i');
+
+  final navigatorKey = GlobalKey<NavigatorState>();
+
+  bool isMenuFixed(BuildContext context) {
+    return MediaQuery.of(context).size.width > 500;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Text(Provider.of<UserProvider>(context).user.name),
-          const SizedBox(
-            height: 20,
+    final theme = Theme.of(context);
+
+    final menu = Container(
+        color: theme.canvasColor,
+        child: SafeArea(
+            right: false,
+            child: Drawer(
+              elevation: 0,
+              child: ListView(
+                children: <Widget>[
+                  for (final s in routes)
+                    ListTile(
+                      title: Text(s),
+                      onTap: () {
+                        // Using navigator key, because the widget is above nested navigator
+                        navigatorKey.currentState!
+                            .pushNamedAndRemoveUntil(s, (r) => false);
+
+                        // navigatorKey.currentState.pushNamed(s);
+                      },
+                    ),
+                ],
+              ),
+            )));
+
+    return Row(
+      children: <Widget>[
+        if (isMenuFixed(context)) menu,
+        Expanded(
+          child: Navigator(
+            key: navigatorKey,
+            onGenerateRoute: (settings) {
+              return MaterialPageRoute(
+                  builder: (context) {
+                    return Scaffold(
+                      appBar: AppBar(
+                        title: Text(settings.name!),
+                      ),
+                      body: SafeArea(
+                        child: Text(settings.name!),
+                      ),
+                      drawer: isMenuFixed(context) ? null : menu,
+                    );
+                  },
+                  settings: settings);
+            },
           ),
-          ElevatedButton(
-              onPressed: () async {
-                final SharedPreferences prefs =
-                    await SharedPreferences.getInstance();
-                prefs.remove("x-auth-token");
-                Navigator.pushNamedAndRemoveUntil(
-                    context, LoginScreen.routeName, (route) => false);
-              },
-              child: Text("Logout")),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
