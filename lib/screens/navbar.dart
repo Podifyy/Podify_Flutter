2c8cf04dcf450d20c4b5ccbcbf3584257625b95c
SHRE-YANSH <itnakim161@gmail.com>
2023-05-08 19:15:18 +0530

    cd

--- lib/screens/navbar.dart
+++ lib/screens/navbar.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:podify/screens/home.dart';
+import 'package:podify/common/custom_textfiled.dart';
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 
 class NavBar extends StatefulWidget {
   const NavBar({super.key});
@@ -10,101 +13,94 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
-  final List<Widget> _screens = [
-    // Content for Home tab
-    const HomeScreen(),
-    // Content for Feed tab
-    Container(
-      color: Colors.purple.shade100,
-      alignment: Alignment.center,
-      child: const Text(
-        'Feed',
-        style: TextStyle(fontSize: 40),
-      ),
-    ),
-    // Content for Favorites tab
-    Container(
-      color: Colors.red.shade100,
-      alignment: Alignment.center,
-      child: const Text(
-        'Favorites',
-        style: TextStyle(fontSize: 40),
-      ),
-    ),
-    // Content for Settings tab
-    Container(
-      color: Colors.pink.shade300,
-      alignment: Alignment.center,
-      child: const Text(
-        'Settings',
-        style: TextStyle(fontSize: 40),
-      ),
-    )
-  ];
+  final TextEditingController _codeController = TextEditingController();
+  final openAI = OpenAI.instance.build(
+    token: "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",
+    baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 5)),
+  );
+  String code = "";
 
-  int _selectedIndex = 0;
+  void optimize() async {
+    try {
+      final request = ChatCompleteText(
+          messages: [
+            {
+              "role": "user",
+              "content":
+                  '${_codeController.text}\noptimze this code with dead code elimination,constant folding and reduction in strength and dont add any explanation in the response '
+            }
+          ],
+          model: ChatModel.gptTurbo, // is model kGPT35Turbo => 'gpt-3.5-turbo'
+          maxToken: 800,
+          temperature: 0.5);
+      final response = await openAI.onChatCompletion(request: request);
+      setState(() {
+        code = response!.choices[0].message!.content;
+        vis = code.isNotEmpty ? true : false;
+      });
+
+      //print(code);
+    } catch (e) {
+      print(e);
+    }
+
+    ///cancel request
+    //print(_codeController.text);
+  }
+
+  bool vis = false;
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width * 0.7;
     return Scaffold(
-      bottomNavigationBar: MediaQuery.of(context).size.width < 640
-          ? BottomNavigationBar(
-              currentIndex: _selectedIndex,
-              unselectedItemColor: Colors.grey,
-              selectedItemColor: Colors.indigoAccent,
-              // called when one tab is selected
-              onTap: (int index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
-              // bottom tab items
-              items: const [
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.home), label: 'Home'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.feed), label: 'Feed'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.favorite), label: 'Favorites'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.settings), label: 'Settings')
-                ])
-          : null,
-      body: Row(
-        children: [
-          if (MediaQuery.of(context).size.width >= 640)
-            SizedBox(
-              width: 200,
-              child: NavigationRail(
-                onDestinationSelected: (int index) {
-                  setState(() {
-                    _selectedIndex = index;
-                  });
-                },
-                selectedIndex: _selectedIndex,
-                destinations: const [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Home'),
-                  ),
-                  NavigationRailDestination(
-                      icon: Icon(Icons.feed), label: Text('Feed')),
-                  NavigationRailDestination(
-                      icon: Icon(Icons.favorite), label: Text('Favorites')),
-                  NavigationRailDestination(
-                      icon: Icon(Icons.settings), label: Text('Settings')),
-                ],
-
-                labelType: NavigationRailLabelType.all,
-                selectedLabelTextStyle: const TextStyle(
-                  color: Colors.teal,
+      backgroundColor: Colors.blueGrey,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Center(
+              child: SizedBox(
+                width: width,
+                child: CustomTextField(
+                  controller: _codeController,
+                  hintText: "code",
+                  maxLines: 20,
                 ),
-
-                unselectedLabelTextStyle: const TextStyle(),
-                // Called when one tab is selected
               ),
             ),
-          Expanded(child: _screens[_selectedIndex])
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                optimize();
+              },
+              child: const Text("Optimize"),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Visibility(
+              visible: vis,
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  color: Colors.lightBlue[100],
+                  width: width,
+                  child: code.isNotEmpty
+                      ? Text(code)
+                      : const Text("Enter the code above"),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
       ),
     );
   }
